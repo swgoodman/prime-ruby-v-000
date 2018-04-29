f34e60a954f84eeef37ac3019cfe98087486c931
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-29 03:32:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def prime?(n)
-  result = false
   if n <= 1
     false
   end
@@ -12,49 +11,5 @@ def prime?(n)
     end
     n += 1
   end
-
-
-
-      range.each do |num|
-        if n % num == 0
-          result = false
-        else
-          result = true
-        end
-      end
-  elsif n == 2
-    result = true
-  else
-    result = false
-  end
-  result
+  true
 end
-
-#  else
-#    false
-#    counter = 2
-#    range = (2..n).to_a
-#      range.each do |num|
-#      if num % counter == 0 && num != counter
-#        false
-#      else
-#        counter += 1
-#        true
-#      end
-#  end
-#nd
-
-
-#  if n > 2
-  #  range = (2..n).to_a
-  #  counter = 2
-  #  n.times do |i|
-    #  range.each do |number|
-    #    if number % counter == 0 && number != counter
-    #    end
-  #    end
-    #  counter += 1
-  #  end
-#    range
-  #end
-#
